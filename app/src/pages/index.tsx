d24e54e2f861831d369c32e5847137575e78da03
Claeb101 <calebarulandu@gmail.com>
2023-01-24 21:40:09 -0500

    faster loading nicer transitions

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -4,15 +4,16 @@ import { useState, useEffect, useLayoutEffect, useRef } from "react";
 import React from "react";
 import { Spinner } from "@/components/Spinner";
 import {Layout} from "@/components/layout";
+import { read } from "fs";
 
-const Choice = ({ fruit, onClick }) => {
+const Choice = ({ fruit, onClick=()=>null, reactive=true }) => {
   return (
-    <div className="bg-slate-lightest bg-opacity-20 w-48 aspect-square m-4 rounded-sm hover:bg-opacity-30 hover:-translate-y-4 transition-all p-4" onClick={onClick}>
+    <div className={`bg-slate-lightest bg-opacity-20 w-48 aspect-square m-4 rounded-sm ${reactive ? "hover:bg-opacity-30 hover:-translate-y-4" : ""} transition-all p-4`} onClick={onClick}>
       {fruit ?
         <>
           <p className="text-white text-lg text-center">{fruit.name} ({Math.round(fruit.rating)})</p>
           <div className="">
-            <img alt="Picture" src={fruit.pic} className="w-full h-full object-cover" />
+            <img alt="Picture" src={fruit.pic} className="w-full h-full object-cover"/>
           </div>
         </>
         :
@@ -25,10 +26,10 @@ const Choice = ({ fruit, onClick }) => {
 
 const Voter = () => {
   const [fruits, setFruits] = useState({ fruit1: null, fruit2: null })
-  const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState<{status: boolean, time: Date}>({status: false, time: null})
 
   const choose = async (ind) => {
-    setReady(false)
+    setReady({status: false, time: new Date()})
     const options = {
       headers: {
         'Content-Type': 'application/json',
@@ -39,35 +40,38 @@ const Voter = () => {
         loser: ind == 0 ? fruits.fruit2.name : fruits.fruit1.name
       })
     }
-
-    const res = await fetch('/api/fruit', options)
-    match()
-    setReady(true)
+    await Promise.all([fetch('/api/fruit', options), match()])
+    
+    const d = new Date()
+    const mt = 2000.
+    const diff = ready.time ? (d.getTime() - ready.time.getTime()) : mt;
+    const timer = setTimeout(() => {
+      setReady({status: true, time: d})
+    }, Math.max(mt-diff, 0))
   }
 
-  const match = () => {
+  const match = async () => {
     const options = {
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'GET'
     }
-    fetch('/api/fruit', options).then(res => res.json()).then((data) => {
-      setFruits({ fruit1: data.fruit1, fruit2: data.fruit2 })
-      setReady(true)
-    })
+    const data = await (await fetch('/api/fruit', options)).json()
+    setFruits({ fruit1: data.fruit1, fruit2: data.fruit2 })
   }
 
   useEffect(() => {
     match()
+    setReady({status: true, time: new Date()})
   }, [])
 
   return (
     <>
-      <Spinner className="absolute w-16 h-16" show={!ready} cycle={2} />
-      <div className={`mt-4 flex flex-col sm:flex-row ${ready ? 'opacity-100' : 'opacity-0'} transition-all`}>
-        <Choice fruit={fruits.fruit1} onClick={() => choose(0)} />
-        <Choice fruit={fruits.fruit2} onClick={() => choose(1)} />
+      <Spinner className="absolute w-16 h-16" show={!ready.status} cycle={2} />
+      <div className={`mt-4 flex flex-col sm:flex-row ${ready.status ? 'opacity-100' : 'opacity-5'} transition-all`}>
+        <Choice fruit={fruits.fruit1} onClick={() => choose(0)} reactive={ready.status}/>
+        <Choice fruit={fruits.fruit2} onClick={() => choose(1)} reactive={ready.status}/>
       </div>
     </>
 
